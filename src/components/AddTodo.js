40e44cddb57008d38b173bdc34054ffcb83bdcf0
Lipika Mohanty <110726592+Lipika07@users.noreply.github.com>
2023-02-07 08:36:15 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,13 +1,24 @@
-import React, {useState} from "react";
+import React, { useRef } from "react"
 
-const AddTodo = ({add}) => {
-  const [text, setText] = useState("");
-  return (
-    <div className="AddTodo">
-      <input value={text} onChange={e => setText(e.target.value)} className="AddTodoInput" />
-      <button className="AddTodoButton" onClick={() => {add(text); setText("")}}>Add</button>
-    </div>
-  );
+const AddTodo = ({ dispatch }) => {
+    const inpRef = useRef();
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        let title = inpRef.current.value;
+        if(!title) return;
+        let id = Date.now();
+        console.log(title,id);
+        dispatch({type : 'addTodo', obj: {title,id}})
+        inpRef.current.value = '';
+    }
+
+    return (
+        <form id="todo-form" onSubmit={handleSubmit}>
+            <label htmlFor="todo-input">Title</label>
+            <input type="text" id="todo-input" ref={inpRef}/>
+            <button type="submit">Add To-Do</button>
+        </form>
+    )
 }
 
-export default AddTodo;
+export { AddTodo }
